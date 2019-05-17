e31a0bc8d22fde8a9c8d2fcf1e4597333e8a48c7
Carlos Quinto <carlquin77@gmail.com>
2019-05-17 14:34:56 -0700

    Initial schema built

---
+++ schema/startInventory.sql
@@ -0,0 +1,10 @@
+USE bamazon;
+
+INSERT INTO products(product_name, department_name, price, stock_quantity)
+VALUES ("'Battle Circuit' Cabinet", "Arcade Hardware", 300.00, 14), ("'Metal Slug X' Cabinet","Arcade Hardware", 700.00, 6),
+("'Battletoads' Cabinet", "Arcade Hardware", 576.00, 10),("'Rage of the Dragons' Cabinet","Arcade Hardware", 800.00, 4),
+("'King of Fighters 98' Cabinet","Arcade Hardware",1200.00, 3),("'Alien Vs Predator' Cabinet", "Arcade Hardware", 3000.00,2),
+("DDR4 RAM Package, 2X8GB MODULES", "PC Components", 85.00, 26), ("1080pi LCD Monitor", "PC Components", 68.00, 31),
+("650 W Modular PSU (100-240 V, 50-60 Hz)", "PC Components", 89.00, 22),
+("Linear Integrated Circuit Voltage Regulator", "Electronic Components",6.50,52),('Dremel 481 3/32" Collet', "Power Tools", 4.00,19),
+("Dremel 953 Grinding Stone", "Power Tools", 5.00, 15);
